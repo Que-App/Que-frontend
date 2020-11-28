5f73cfab65ba761b647a8d4e9bf649a2a8f82722
Piotr <piotr.wiorek71@gmail.com>
2020-11-28 16:46:53 +0100

    Angular Material imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module'
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+  imports: [
+    MatIconModule,
+  ],
+  exports: [
+    MatIconModule
+  ]
+})
+export class MaterialModule {}
