04e7812cfbaefc9888678b7c991d45bc21a2191f
Piotr <piotr.wiorek71@gmail.com>
2021-01-15 18:24:20 +0100

    Useless components deleted

--- src/app/shared/components/hour-printer.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HourPrinterPipe } from './hour-printer.pipe';
-
-describe('HourPrinterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new HourPrinterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,20 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MaterialModule } from '../material.module';
-import { SelectorComponent } from './components/selector/selector.component';
-import { TileComponent } from './components/tile/tile.component';
 import { NavbarPathConverterPipe } from './pipes/navbar-path-converter.pipe';
 
 
 @NgModule({
-  declarations: [NavbarComponent, SelectorComponent, TileComponent, NavbarPathConverterPipe],
+  declarations: [NavbarComponent, NavbarPathConverterPipe],
   imports: [
     CommonModule,
     MaterialModule
   ],
   exports: [
     NavbarComponent,
-    TileComponent
   ],
   providers: [
     NavbarPathConverterPipe
