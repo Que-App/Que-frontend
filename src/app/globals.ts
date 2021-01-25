48eb163445c26a8d3710f76febe00341e6a0c143
Piotr <piotr.wiorek71@gmail.com>
2021-01-25 15:40:20 +0100

    Api url changed

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -2,6 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Globals {
-  BASE_URL = 'http://localhost:8080/api/v1';
+  BASE_URL = 'https://queapi.jupw.net/api/v1';
   VERSION = '1.0.0';
 }
